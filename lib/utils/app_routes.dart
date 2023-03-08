bfd28b750887ced02a37cd1828fc8b54926f1dce
Gustavo Gama <gustavofarewell96@gmail.com>
2023-03-08 00:44:13 -0300

    Meals

---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  static const home = '/';
+  static const categoriesMeals = '/categories-meals';
+  static const mealsDetail = '/meals-detail';
+  static const settings = '/settings';
+}
